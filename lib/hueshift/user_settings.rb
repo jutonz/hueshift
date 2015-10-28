f9ccb6e94ac74b373c4d5601392cef0bb69d3d61
Justin Toniazzo <jutonz42@gmail.com>
2015-10-28 19:58:50 -0400

    Correctly loads user settings from ~/.hueshift
    Was previously loading just .hueshift

--- lib/hueshift/user_settings.rb
+++ lib/hueshift/user_settings.rb
@@ -1,11 +1,16 @@
 module Hueshift
   USER_SETTINGS_FILE = ".hueshift"
+  USER_SETTINGS_PATH = File.expand_path USER_SETTINGS_FILE, Dir.home
 
+  ##
+  # Load the settings in USER_SETTINGS_PATH, if it exists.
+  #
+  # @return whether USER_SETTINGS_PATH exists
   def load_user_settings
-    settings_file = File.expand_path USER_SETTINGS_FILE, Dir.home
-    if File.exist? settings_file
+    if (exists = File.exist? USER_SETTINGS_PATH)
       require "dotenv"
-      Dotenv.load settings_file
+      Dotenv.load USER_SETTINGS_PATH
     end
+    exists
   end
 end
--- lib/hueshift/version.rb
+++ lib/hueshift/version.rb
@@ -1,3 +1,3 @@
 module Hueshift
-  VERSION = "0.3.1"
+  VERSION = "0.3.2"
 end
